9755523eb546688824e4755c555c71a7b25e35d6
RJD02 <dulangeraviraj@gmail.com>
2023-07-27 01:47:43 +0530

    Add search bar and movie template

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const searchTerm = document.querySelector("#search");
 
-const omdbURL = "http://www.omdbapi.com/";
+const omdbURL = "https://www.omdbapi.com/";
 const API_KEY = "9e0f94a9";
 
-const favMovies = [];
-searchTerm.addEventListener("keyup", () => {
+let favMovies = [];
+let currMovies = [];
+
+searchTerm.addEventListener("keyup", async () => {
   let searchText = searchTerm.value;
   if (searchText === "") return;
-  const movies = fetchMovies(searchText);
-  // showMovies(movies);
+  currMovies = await fetchMovies(searchText);
+  console.log("Current movies", currMovies);
 });
 
 const fetchMovies = async (searchText) => {
   const url = new URL(omdbURL);
   url.searchParams.set("apikey", API_KEY);
-  url.searchParams.set("t", searchText);
+  url.searchParams.set("s", searchText);
   console.log(url);
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     return data;
   } catch (e) {
     console.log(e);
