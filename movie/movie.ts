ef0cf35d7050e6bb1961b2328b30f0003d3a9c0f
RJD02 <dulangeraviraj@gmail.com>
2023-08-01 20:05:12 +0530

    Make add to fav btn and add functionality

--- movie/movie.ts
+++ movie/movie.ts
@@ -49,7 +49,15 @@ interface OMDBSearchByIdResponse {
 const showDetails = async () => {
   const movieId = localStorage.getItem("movieId");
   console.log(movieId);
-  if (movieId == null || movieId == undefined) return;
+  if (movieId == null || movieId == undefined) {
+    console.log("No movieId found from local storage");
+    const warningText = document.querySelector(".warning");
+    if (!warningText) return;
+    warningText.classList.remove("invisible");
+    warningText.textContent =
+      "Hey, seems like you cleared local storage. We can't have 2 developers here, aight?";
+    return;
+  }
   try {
     const movie: OMDBSearchByIdResponse = await getMovieById(movieId);
     if (!movie) return;
@@ -91,14 +99,16 @@ const showDetails = async () => {
                 </div>
                 <div class="cta-btn flex mt-8 items-center">
                   <button
-                    class="rounded-md bg-amber-500 px-2 py-1 mr-3 text-xl"
+                    class="flex items-center rounded-full hover:bg-amber-600 bg-amber-500 px-5 py-3 mr-3 text-base text-gray-100"
                   >
-                    Watch now
+                  <i class="fa-solid fa-play fa-lg mr-1"></i>
+                    <span>Watch now</span>
                   </button>
                   <button
-                    class="rounded-sm bg-white text-black px-2 py-1 text-xl"
+                    class="flex fav-btn items-center rounded-full hover:bg-slate-200 bg-white text-black px-2 py-3 text-base"
                   >
-                    Add to List
+                  <span><i class="fa-solid fa-plus fa-lg mr-1"></i></span>
+                   <span>Add to Favorites</span> 
                   </button>
                 </div>
                 <div class="plot mt-8">
@@ -161,9 +171,21 @@ const showDetails = async () => {
           </div>
         `.trim();
     const containerDiv = document.querySelector(".container");
-    const loadingDiv = document.querySelector(".loading");
     containerDiv?.append(movieDetailDiv);
+    const loadingDiv = document.querySelector(".loading");
+    const addToFavBtn = document.querySelector(".fav-btn");
+    console.log(addToFavBtn);
+    addToFavBtn?.addEventListener("click", () => {
+      const jsonFavlist = localStorage.getItem("favList");
+      if (jsonFavlist == null || jsonFavlist == undefined) return;
+      const favMovieList: string[] = JSON.parse(jsonFavlist);
+      console.log(favMovieList);
+      favMovieList.push(movie.imdbID);
+
+      localStorage.setItem("favList", JSON.stringify(favMovieList));
+    });
     loadingDiv?.classList.add("invisible");
+
     window.addEventListener("beforeunload", () => {
       movieDetailDiv.remove();
     });
